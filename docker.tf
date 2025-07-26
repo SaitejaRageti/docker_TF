8f2088c4066b6e22c55cc1e31e8d1bbb76c148de
saitejarageti <saitejarageti129@gmail.com>
2025-07-26 16:18:03 +0530

    docker

--- docker.tf
+++ docker.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "docker" {
   ami           = local.ami_id
-  instance_type = "t3.micro"
+  instance_type = "t3.medium"
 
   vpc_security_group_ids = [aws_security_group.docker.id]
 
@@ -27,11 +27,12 @@ resource "aws_security_group" "docker" {
   description = "security gtoup for the docker instance"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    }
 
   egress {
     from_port   = 0
